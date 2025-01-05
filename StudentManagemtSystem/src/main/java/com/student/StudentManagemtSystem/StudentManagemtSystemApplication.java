17799555bc4f5d4e970a573504a1f88230edd591
AbhinayYendoti <abhiyendoti@gmail.com>
2025-01-05 21:37:39 +0530

    Add files via upload

---
+++ StudentManagemtSystem/src/main/java/com/student/StudentManagemtSystem/StudentManagemtSystemApplication.java
@@ -0,0 +1,13 @@
+package com.student.StudentManagemtSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagemtSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagemtSystemApplication.class, args);
+	}
+
+}
---
+++ StudentManagemtSystem/src/test/java/com/student/StudentManagemtSystem/StudentManagemtSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.student.StudentManagemtSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagemtSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
